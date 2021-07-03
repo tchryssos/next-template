6fa2bb5e4f57c17f3f15fdcb815f94a14febea25
Troy Chryssos <troy@codecademy.com>
2021-07-02 22:11:00 -0400

    updating eslint to use words rather than numbers

--- src/logic/utils/ternary.ts
+++ src/logic/utils/ternary.ts
@@ -1,9 +1,8 @@
 const ternary = (bool: boolean, truthy: unknown, falsey: unknown) => {
-	if (bool) {
-		return truthy
-	}
-	return falsey
-}
+  if (bool) {
+    return truthy;
+  }
+  return falsey;
+};
 
-export default ternary
-
+export default ternary;
