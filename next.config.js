d281f5e7806c1cfd73daef3c84dc353e4cec790f
Troy Chryssos <troy@codecademy.com>
2023-04-06 22:45:21 -0400

    updating next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   eslint: {
     dirs: ['pages', 'utils', 'constants', 'components', 'typings'],
   },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  compiler: {
+    emotion: true,
+  },
 };
